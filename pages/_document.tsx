90cbc3482068cb62513e3c7f3f9ae23a52bb4f16
Lorenzo Davoli <lorenzo.davoli@webranking.it>
2023-04-11 12:58:12 +0200

    nascosti elementi ui + inclusione script gtm

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,6 @@
 import { Html, Head, Main, NextScript, DocumentProps } from 'next/document';
 import i18nextConfig from '../next-i18next.config';
+import Script from 'next/script';
 
 type Props = DocumentProps & {
   // add custom document props
@@ -13,6 +14,19 @@ export default function Document(props: Props) {
       <Head>
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-title" content="Chatbot UI"></meta>
+        <Script
+          id="gtm"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-59VBGGD');
+`,
+          }}
+        ></Script>
       </Head>
       <body>
         <Main />
